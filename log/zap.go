6fec6768b2265f48b9ddcb8bb4f278ace1dc8863
沈浩 <sh.maranello@gmail.com>
2024-12-24 12:57:28 +0800

    feat: add log file

--- log/zap.go
+++ log/zap.go
@@ -1,15 +1,31 @@
 package log
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
 
 var Logger *zap.Logger
 
 func InitLogger() {
-	var err error
-	Logger, err = zap.NewProduction()
+	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		panic("无法初始化日志： " + err.Error())
+		panic("无法打开日志文件: " + err.Error())
 	}
+
+	fileSyncer := zapcore.AddSync(file)
+	consoleSyncer := zapcore.AddSync(os.Stdout)
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(zapcore.NewJSONEncoder(encoderConfig), fileSyncer, zap.DebugLevel),      // 写入文件（JSON 格式）
+		zapcore.NewCore(zapcore.NewConsoleEncoder(encoderConfig), consoleSyncer, zap.InfoLevel), // 输出到终端
+	)
+
+	Logger = zap.New(core, zap.AddCaller())
 }
 
 func GetLogger() *zap.Logger {
