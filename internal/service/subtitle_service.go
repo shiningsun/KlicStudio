6b1b22bb98aa14b327a257cf24c521cc827aebd1
沈浩 <sh.maranello@gmail.com>
2025-05-30 18:51:46 +0800

    fix: ffmpeg error caused by task path

--- internal/service/subtitle_service.go
+++ internal/service/subtitle_service.go
@@ -34,6 +34,7 @@ func (s Service) StartSubtitleTask(req dto.StartVideoSubtitleTaskReq) (*dto.Star
 	// 生成任务id
 	seperates := strings.Split(req.Url, "/")
 	taskId := fmt.Sprintf("%s_%s", util.SanitizePathName(string([]rune(strings.ReplaceAll(seperates[len(seperates)-1], " ", ""))[:16])), util.GenerateRandStringWithUpperLowerNum(4))
+	taskId = strings.ReplaceAll(taskId, "=", "") // 等于号影响ffmpeg处理
 	// 构造任务所需参数
 	var resultType types.SubtitleResultType
 	// 根据入参选项确定要返回的字幕类型
